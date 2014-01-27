46269c7605d78b12c61b54bf1ddcb8907c7185a3
kecinzer <kecinzer@gmail.com>
2014-01-27 12:02:15 +0100

    Tunables for franco.Kernel
    Signed-off-by: kecinzer <kecinzer@gmail.com>

--- sbin/fkbootscript.sh
+++ sbin/fkbootscript.sh
@@ -3,7 +3,7 @@
 # disable sysctl.conf to prevent ROM interference with tunables
 # backup and replace PowerHAL with custom build to allow OC/UC to survive screen off
 # create and set permissions for /system/etc/init.d if it doesn't already exist
-mount -o rw,remount /system;
+mount -o rw,remount /system /system;
 [ -e /system/etc/sysctl.conf ] && mv /system/etc/sysctl.conf /system/etc/sysctl.conf.fkbak;
 [ -f /system/bin/thermal-engine-hh-bak ] || mv /system/bin/thermal-engine-hh /system/bin/thermal-engine-hh-bak
 if [ ! -e /system/etc/init.d ]; then
@@ -11,6 +11,9 @@ if [ ! -e /system/etc/init.d ]; then
   chown -R root.root /system/etc/init.d;
   chmod -R 755 /system/etc/init.d;
 fi;
-mount -o ro,remount /system;
+mount -o ro,remount /system /system;
 
-echo 90 1574400:90 1958400:99 > /sys/devices/system/cpu/cpufreq/interactive/target_loads
+echo 85 1500000:90 1800000:70 > /sys/devices/system/cpu/cpufreq/interactive/target_loads
+echo 20000 1400000:40000 1700000:20000 > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
+
+echo 2 > /sys/devices/system/cpu/sched_mc_power_savings
